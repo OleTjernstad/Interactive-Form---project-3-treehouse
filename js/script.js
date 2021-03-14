845b41f93bea1d7c39105b76abbfc71340a45e04
Ole Tjernstad <ott556@gmail.com>
2021-03-14 21:52:06 +0100

    only display selected payment option

--- js/script.js
+++ js/script.js
@@ -34,7 +34,6 @@ const activitiesCost = document.querySelector("#activities-cost");
 let totalCost = 0;
 
 activities.addEventListener("change", (event) => {
-    console.log(event.target)
     if (event.target.type == 'checkbox') {
         if (event.target.checked) {
             totalCost += parseInt(event.target.dataset.cost);
@@ -43,5 +42,49 @@ activities.addEventListener("change", (event) => {
         }
 
         activitiesCost.textContent = `Total: $${totalCost}`;
+    }
+});
+
+const payment = document.querySelector("#payment");
+
+
+payment.addEventListener("change", (event) => {
+   
+    switch (event.target.value) {
+      case "credit-card":
+          for (let child of payment.parentElement.parentElement.children) {
+              if (child.className != "payment-method-box") {
+                if (child.id == "credit-card") {
+                  child.style.display = "block";
+                } else {
+                  child.style.display = "none";
+                }
+            }
+          }
+        break;
+      case "paypal":
+           for (let child of payment.parentElement.parentElement.children) {
+             if (child.className != "payment-method-box") {
+               if (child.id == "paypal") {
+                 child.style.display = "block";
+               } else {
+                 child.style.display = "none";
+               }
+             }
+           }
+        break;
+      case "bitcoin":
+           for (let child of payment.parentElement.parentElement.children) {
+             if (child.className != "payment-method-box") {
+               if (child.id == "bitcoin") {
+                 child.style.display = "block";
+               } else {
+                 child.style.display = "none";
+               }
+             }
+           }
+        break; 
+      default:
+        break;
     }
 });
