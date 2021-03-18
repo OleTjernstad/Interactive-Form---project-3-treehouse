c664fc85ddd61cabc32a7c485ac0555710760e29
Ole Tjernstad <ott556@gmail.com>
2021-03-18 18:45:52 +0100

    email varification with different messages

--- js/validation.js
+++ js/validation.js
@@ -14,32 +14,39 @@ const _isValidNumber = (number, length) => {
     return regex.test(number);
 }
 
-const _displayNotValid = (element) => {
+const _displayNotValid = (element, hintSelector) => {
     element.classList.add("not-valid");
     element.classList.remove("valid");
-    element.lastElementChild.style.display = "block"; 
+    element.querySelector(hintSelector).style.display = "block"; 
 }
-const _removeNotValid = (element) => {
+const _removeNotValid = (element, hintSelector) => {
     element.classList.remove("not-valid");
     element.classList.add("valid");
-    element.lastElementChild.style.display = "none"; 
+    element.querySelector(hintSelector).style.display = "none"; 
 }
 
 const validateNameField = (element) => {
     if (_isNotEmpty(element.value)) {
-        _removeNotValid(element.parentElement);
+        _removeNotValid(element.parentElement, "#name-hint");
         return true;
     }
-    _displayNotValid(element.parentElement);
+    _displayNotValid(element.parentElement, "#name-hint");
     return false;
 };
 
 const validateEmailField = (element) => {
+
+    if (_isNotEmpty(element.value)) {
+        _removeNotValid(element.parentElement, "#email-hint-provided");
+    } else {
+        _displayNotValid(element.parentElement, "#email-hint-provided");
+        return false;
+    }
     if (_isValidEmail(element.value)) {
-        _removeNotValid(element.parentElement);
+        _removeNotValid(element.parentElement, "#email-hint");
         return true;
     }
-    _displayNotValid(element.parentElement);
+    _displayNotValid(element.parentElement, "#email-hint");
     return false;
 };
 
@@ -52,34 +59,46 @@ const validateActivities = (elements) => {
     }
 
     if(selectedItems.length < 1) {
-        _displayNotValid(elements[0].parentElement.parentElement.parentElement);
+        _displayNotValid(
+          elements[0].parentElement.parentElement.parentElement,
+          "#activities-hint"
+        );
         return false;
     } 
-    _removeNotValid(elements[0].parentElement.parentElement.parentElement);
+    _removeNotValid(
+      elements[0].parentElement.parentElement.parentElement,
+      "#activities-hint"
+    );
     return true;
 };
-const _showOrHideErrorForPayment = (element, valid) => {
+const _showOrHideErrorForPayment = (element, valid, hintSelector) => {
     if (valid) {
-        _removeNotValid(element.parentElement);
+        _removeNotValid(element.parentElement, hintSelector);
     } else {
-        _displayNotValid(element.parentElement);
+        _displayNotValid(element.parentElement, hintSelector);
     }
     return valid;
 }
 const validateCVV = (CVV) => {
     return _showOrHideErrorForPayment(
-        CVV, _isValidNumber(CVV.value, 3)
+      CVV,
+      _isValidNumber(CVV.value, 3),
+      "#cvv-hint"
     );
 }
 const validateZip = (zipCode) => {
     return _showOrHideErrorForPayment(
-          zipCode, _isValidNumber(zipCode.value, 5)
-        );
+      zipCode,
+      _isValidNumber(zipCode.value, 5),
+      "#zip-hint"
+    );
 }
 const validateCardNum = (cardNum) => {
     return _showOrHideErrorForPayment(
-          cardNum, _isValidNumber(cardNum.value, "13,16")
-        );
+      cardNum,
+      _isValidNumber(cardNum.value, "13,16"),
+      "#cc-hint"
+    );
 }
 const validatePayment = (cardNum, zipCode, CVV, selectedPaymentType) => {
     if (selectedPaymentType === "credit-card") {
